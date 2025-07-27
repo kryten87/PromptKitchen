b0361be50dd5f27fed8af1ade7b29cd15925d3ee
Dave Thomas <dave@monstergfx.com>
2025-07-27 15:21:38 -0700

    chore: scaffold front end

---
+++ packages/frontend/src/pages/AboutPage.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const AboutPage: React.FC = () => {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">
+        About Page
+      </h1>
+    </div>
+  );
+};
+
+export default AboutPage;

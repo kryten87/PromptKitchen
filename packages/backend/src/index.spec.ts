01dfc676dd292e881ee229173326f76cc553f462
Dave Thomas <dave@monstergfx.com>
2025-07-27 15:33:29 -0700

    chore: clean up npm commands in packages

---
+++ packages/backend/src/index.spec.ts
@@ -0,0 +1,5 @@
+describe('backend', () => {
+  it('should work', () => {
+    expect(1).toBe(1);
+  });
+});
---
+++ packages/frontend/src/App.spec.tsx
@@ -0,0 +1,5 @@
+describe('frontend', () => {
+  it('should work', () => {
+    expect(1).toBe(1);
+  });
+});

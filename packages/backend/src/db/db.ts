80f95771d1fca8c0df738d1b759f2c5c8843b2ef
Dave Thomas <dave@monstergfx.com>
2025-07-28 16:24:07 -0700

    refactor: convert db connection to class

--- packages/backend/src/db/db.ts
+++ packages/backend/src/db/db.ts
@@ -1,13 +1,25 @@
-import knex from 'knex';
+import knex, { Knex } from 'knex';
 
+export interface DatabaseConnectorConfig {
+  filename: string;
+}
+
+export class DatabaseConnector {
+  public readonly knex: Knex;
+
+  constructor(config: DatabaseConnectorConfig) {
+    this.knex = knex({
+      client: 'sqlite3',
+      connection: { filename: config.filename },
+      useNullAsDefault: true,
+    });
+  }
+
+  async destroy() {
+    await this.knex.destroy();
+  }
+}
+
+// For production usage, export a singleton instance using env config
 const dbFile = process.env.DB_FILE || './dev.sqlite3';
-
-export const promptKitchenDb = knex({
-  client: 'sqlite3',
-  connection: {
-    filename: dbFile,
-  },
-  useNullAsDefault: true,
-});
-
-export { promptKitchenDb as knex };
+export const prodDbConnector = new DatabaseConnector({ filename: dbFile });

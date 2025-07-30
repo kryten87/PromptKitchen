883c74396dd44bfbf21001d9329843da5a11776b
Dave Thomas <dave@monstergfx.com>
2025-07-30 16:07:29 -0700

    feat: add App component with router

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -1,16 +1,24 @@
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import { AppLayout } from './AppLayout';
 import AboutPage from './pages/AboutPage';
 import HomePage from './pages/HomePage';
+import { SessionProvider } from './SessionContext';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/about" element={<AboutPage />} />
-      </Routes>
-    </Router>
+    <SessionProvider>
+      <Router>
+        <AppLayout
+          sidebar={<div className="p-4 font-bold">Prompt Kitchen</div>}
+        >
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/about" element={<AboutPage />} />
+          </Routes>
+        </AppLayout>
+      </Router>
+    </SessionProvider>
   );
 }
 
-export default App;
+export { App };

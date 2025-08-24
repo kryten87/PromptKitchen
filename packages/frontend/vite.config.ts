8637dac1aced1d9769b002ac8eefcbb5db5ce176
Dave Thomas <dave@monstergfx.com>
2025-08-24 12:03:48 -0700

    fix: improve vite proxy config

--- packages/frontend/vite.config.ts
+++ packages/frontend/vite.config.ts
@@ -16,7 +16,25 @@ export default defineConfig({
   },
   server: {
     proxy: {
-      '/api': 'http://localhost:3000',
+      '/api': {
+        target: 'http://localhost:3000',
+        changeOrigin: true,
+        configure: (proxy, _options) => {
+          proxy.on('error', (err, _req, _res) => {
+            console.log('proxy error', err);
+          });
+          proxy.on('proxyReq', (proxyReq, req, _res) => {
+            console.log('Sending Request to the Target:', req.method, req.url);
+          });
+          proxy.on('proxyRes', (proxyRes, req, _res) => {
+            console.log(
+              'Received Response from the Target:',
+              proxyRes.statusCode,
+              req.url
+            );
+          });
+        },
+      },
       '/auth': 'http://localhost:3000',
     },
   },
